b6079c60f661c4aedca2180ae5d8ed8e826cb6be
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-10-23 18:16:40 +0900

    ログインの制御を追加

--- oauth_login/app/controllers/home_controller.rb
+++ oauth_login/app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    unless session[:user_name]
+      redirect_to new_session_url
+    end
   end
 end
--- oauth_login/config/routes.rb
+++ oauth_login/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match   '/auth/:provider/callback', to: 'oauth_callbacks#create', via: [:get, :post], as: :oauth_callbacks
 
+  get 'session/new', to: 'sessions#new', as: :new_session
   root 'home#index'
 end
