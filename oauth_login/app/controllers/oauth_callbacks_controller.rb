aff300f13830442ec920709293a55f72f2e1efdf
ShoichiroKitano <shoichiro.kitano@gaiax.com>
2016-10-23 18:47:34 +0900

    viewを本実装

--- oauth_login/app/controllers/oauth_callbacks_controller.rb
+++ oauth_login/app/controllers/oauth_callbacks_controller.rb
@@ -1,11 +1,14 @@
 class OauthCallbacksController < ApplicationController
 
   def create
-    users = User.where(uid: '1234567890')
+    auth_hash = request.env['omniauth.auth']
+    uid = auth_hash['uid']
+    users = User.where(uid: uid)
     if users.any?
       session[:user_name] = users[0].name
     else
-      new_user = User.create!(uid: '1234567890', name: 'ユーザの名前', email: 'user@gmail.com')
+      info = auth_hash['info']
+      new_user = User.create!(uid: uid, name: info['name'], email: info['email'])
       session[:user_name] = new_user.name
     end
     redirect_to root_url
