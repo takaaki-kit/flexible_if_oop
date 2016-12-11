dcc97328743914a1faddd9ca5e9aa87a04a0b9bd
takaaki.konaka <takaaki.konaka@gaiax.com>
2016-12-11 17:03:16 +0900

    initial

---
+++ flexible_if/src/Main.java
@@ -0,0 +1,9 @@
+import unko.Aho;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("wwwwwww");
+        Aho aho = new Aho("unkooooooooooo");
+        aho.boke();
+    }
+}
