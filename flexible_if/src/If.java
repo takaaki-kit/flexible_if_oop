9cc34ef460b75c3c896ad5b77c22e2607f491b1a
Takaaaki_z <takaaki.kit@gmail.com>
2017-01-04 21:40:14 +0900

    不要なコンストラクタを削除

--- flexible_if/src/If.java
+++ flexible_if/src/If.java
@@ -1,15 +1,13 @@
 public class If {
     Condition condition;
 
-    public If() {
-    }
-
     public void create(Condition condition) {
         this.condition = condition;
     }
 
     public void exec(int number) {
         //もし条件式の実行結果がtrueであれば、ifブロックの中のステートメントを実行させる
+        //フレームワーク側でifステートメントを使うのってありやっけ？
         if(this.condition.run(number)) {
             this.condition.result();
         }
--- flexible_if/src/MultipleOfThree.java
+++ flexible_if/src/MultipleOfThree.java
@@ -1,7 +1,4 @@
 public class MultipleOfThree extends Condition{
-    public MultipleOfThree() {
-    }
-
     public boolean run(int number) {
         System.out.println("exec multiple of three");
         return number % 3 == 0;
