4fd1cc0032dda550054bd19fe90ced07ab719d72
Takaaaki_z <takaaki.kit@gmail.com>
2017-01-04 21:38:16 +0900

    不要なbooleanの戻り値をなくした

--- flexible_if/src/If.java
+++ flexible_if/src/If.java
@@ -8,11 +8,10 @@ public class If {
         this.condition = condition;
     }
 
-    public boolean exec(int number) {
+    public void exec(int number) {
         //もし条件式の実行結果がtrueであれば、ifブロックの中のステートメントを実行させる
         if(this.condition.run(number)) {
             this.condition.result();
         }
-        return true;
     }
 }
